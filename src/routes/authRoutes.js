cc9971494d6a26d53d85aa1c5e9d5815a089b97e
ayudyanandira <ayudyanandira@gmail.com>
2026-05-18 02:05:34 +0700

    logout endpoint

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,10 @@
-const router = require('express').Router();
-const c = require('../controllers/authController');
-const auth = require('../middleware/auth');
+const router = require("express").Router();
+const c = require("../controllers/authController");
+const auth = require("../middleware/auth");
 
-router.post('/register', c.register);
-router.post('/login', c.login);
-router.get('/me', auth, c.me);
+router.post("/register", c.register);
+router.post("/login", c.login);
+router.get("/me", auth, c.me);
+router.post("/logout", auth, c.logout);
 
 module.exports = router;
